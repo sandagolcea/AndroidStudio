c81fac1424fe38618870687e4b37bdc44f193f8b
Sanda Golcea <sgolcea@deloitte.co.uk>
2017-11-02 20:47:15 +0000

    Added more cookie monsters

--- app/src/main/java/com/codebar/sgolcea/cookiemonster/MainActivity.java
+++ app/src/main/java/com/codebar/sgolcea/cookiemonster/MainActivity.java
@@ -11,12 +11,24 @@ public class MainActivity extends AppCompatActivity {
 
     private Integer currentCount = 0;
 
+    private Integer monsterType() {
+        // >= 20 blue monster android:src="@drawable/cookie"
+        // >= 10 cookie <= 20
+        // < 10 green monster
+        if (currentCount >= 20) {
+            return R.drawable.blue_cookie_monster;
+        } else if (currentCount >= 10) {
+            return R.drawable.green_cookie_monster;
+        }
+        return R.drawable.cookie;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageView imgCookie = (ImageView)findViewById(R.id.imgCookie);
+        final ImageView imgCookie = (ImageView)findViewById(R.id.imgCookie);
         final TextView counter = (TextView)findViewById(R.id.counter);
 
         imgCookie.setOnClickListener(new View.OnClickListener() {
@@ -25,6 +37,8 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "hola!", Toast.LENGTH_SHORT).show();
                 currentCount++;
                 counter.setText(""+currentCount);
+
+                imgCookie.setImageResource(monsterType());
             }
         });
     }
